4bbd3529086b7f37b0738e8e40d6154fd9982cea
Adan Mercado <adanmercado.dev@gmail.com>
2022-06-27 01:02:55 -0500

    Add application data

--- src/main.py
+++ src/main.py
@@ -6,6 +6,10 @@ from window import Window
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setApplicationName('nationalizeapi client')
+    app.setApplicationDisplayName('nationalizeapi client')
+    app.setApplicationVersion('1.0')
+    app.setOrganizationName('adanmercado')
 
     window = Window()
     window.show()
